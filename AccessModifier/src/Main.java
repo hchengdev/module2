c69002c22b381b2e6072aabaf9322078cc6531a1
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-12 15:37:55 +0700

    circle

---
+++ AccessModifier/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Circle TestCircle = new Circle(3.8, "red");
+        System.out.println(TestCircle.getArea());
+        System.out.println(TestCircle.getRadius());
+    }
+}
