0c895a0a304ca3aa29138729e8393f7c3eaa80ac
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-12 15:54:20 +0700

    class

---
+++ Class/src/Class.java
@@ -0,0 +1,29 @@
+public class Class {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Class() {
+    }
+
+    public Class(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+}
---
+++ Class/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Class aClass = new Class();
+        Class bClass = new Class("1", "1");
+        System.out.println(bClass.getClasses());
+    }
+}
