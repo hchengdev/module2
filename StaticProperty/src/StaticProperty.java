52630f668a56ee2642b52af29b47426ef5039efe
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-12 15:20:05 +0700

    car

---
+++ StaticProperty/src/StaticProperty.java
@@ -0,0 +1,34 @@
+public class StaticProperty {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+    public StaticProperty(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public static int getNumberOfCars() {
+        return numberOfCars;
+    }
+
+    public static void setNumberOfCars(int numberOfCars) {
+        StaticProperty.numberOfCars = numberOfCars;
+    }
+}
