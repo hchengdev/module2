e8cdb003026a0967e8d84697276f7a8e77ee854c
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-13 16:42:55 +0700

    Kethuab3

--- Circle-Cylinder/src/Main.java
+++ Circle-Cylinder/src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Circle cr = new Circle(10 ,"red");
+        System.out.println(cr.getArea());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Circle cr2 = new Cylinder(20 ,"red", 14);
+        Cylinder cr3 = (Cylinder) cr2;
+        System.out.println(Math.ceil(cr3.getvolume()));
     }
 }
---
+++ Point2D-Point3D/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+      Point3D p3d = new Point3D();
+      Point2D p2d = new Point2D();
+
+        System.out.println(p3d.toString());
+        System.out.println(p2d.toString());
+    }
+}
