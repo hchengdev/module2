9f6e2e8506617ff5ea860aca82d30967e3f82864
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-13 15:06:14 +0700

    keThuaB2

---
+++ Circle-Cylinder/src/Circle.java
@@ -0,0 +1,37 @@
+public class Circle {
+    private double radius;
+    private String color;
+    public Circle() {}
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
