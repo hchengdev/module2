6b4d6bd1d45543ca4988b229ad34dab53ff021d8
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-20 14:27:39 +0700

    linkerList

---
+++ DSAp2/LinkedListp2/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        MyGenericStack<String> stack = new MyGenericStack();
+        stack.push("Five");
+        stack.push("Four");
+        stack.push("Three");
+        stack.push("Two");
+        stack.push("One");
+        System.out.println("1.1 Size of stack after push operations: " + stack.size());
+        System.out.printf("1.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %s", stack.pop());
+        }
+        System.out.println("\n1.3. Size of stack after pop operations : " + stack.size());
+    }
+}
