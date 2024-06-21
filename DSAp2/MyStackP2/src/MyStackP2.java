03951f2353b6fcc853e933e0dac26876bbc06f36
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-21 11:11:19 +0700

    StackP2

---
+++ DSAp2/MyStackP2/src/MyStackP2.java
@@ -0,0 +1,35 @@
+public class MyStackP2 {
+    private int[] arr;
+    private int size;
+    private int index = 0;
+    public MyStackP2(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+    public void push(int x) {
+        if (isFull()) {
+            System.out.println("Stack is full");
+        }
+        arr[index] = x;
+        index++;
+    }
+
+    public int pop() {
+        if(isEmpty()) {
+            System.out.println("Stack is empty");
+        }
+    }
+
+    public boolean isEmpty() {
+        return index == 0;
+    }
+
+    public boolean isFull() {
+        return index == size;
+    }
+
+    public int size() {
+        return index;
+    }
+
+}
