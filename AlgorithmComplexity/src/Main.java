dbd9f5beae6850b18fe91104160b83ddbe3934b7
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-24 14:31:55 +0700

    Charmax

---
+++ AlgorithmComplexity/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+
+        int[] frequentChar = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            int ascii = (int) inputString.charAt(i);
+            frequentChar[ascii]++;
+        }
+
+        int max = 0;
+        char character = (char) 255;
+        for (int i = 0; i < frequentChar.length; i++) {
+            if (frequentChar[i] > max) {
+                max = frequentChar[i];
+                character = (char) i;
+            }
+        }
+        System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+
+    }
+}
