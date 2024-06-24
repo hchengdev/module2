1b633c5bd3706480461260e9387c5616b37e627a
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-06-24 11:50:59 +0700

    BinarySearch

---
+++ ThuatToanTimKiem/BinarySearch/src/BinarySearch.java
@@ -0,0 +1,20 @@
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
